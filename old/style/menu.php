f4bc18de0b3efe32a3ef4fb0c306a2f66efdffd7
Karl Leswing <lilleswing@gmail.com>
2015-07-13 08:52:47 -0400

    Clearing out old website

--- old/style/menu.php
+++
@@ -1,8 +0,0 @@
-                    
-				<div id="menu">
-                    <ul>
-						<li class="first"><a href="http://karlleswing.com/index.php">Home</a></li>
-						<li><a href="http://karlleswing.com/bio.php">Bio</a></li>
-						<li class="last"><a href="http://karlleswing.com/contact.php">Contact</a></li>
-					</ul>
-					<br class="clearfix" />
