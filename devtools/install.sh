9becb1dc4001f11aeaabc12e44c7823ea867acf0
Karl Leswing <karl.leswing@gmail.com>
2020-12-29 15:29:18 -0500

    GOGO

--- devtools/install.sh
+++ devtools/install.sh
@@ -37,7 +37,11 @@ echo "Installed $ENV_NAME conda environment"
 
 
 ### Front End
-which gem
+wget -O rvm.sh https://get.rvm.io
+bash rvm.sh --path `pwd`/.rvm
+source "`pwd`/rvm/scripts/rvm"
+rvm install ruby-3.0.0
+rvm use 3.0.0
 gem update --system
 gem install bundler
 bundle install
