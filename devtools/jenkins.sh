c60657851536eca45c7b462ac61c1c215f78872e
Karl Leswing <karl.leswing@gmail.com>
2024-11-24 15:42:37 -0500

    Deploy

--- devtools/jenkins.sh
+++ devtools/jenkins.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -xe
 bash devtools/set_docker_user.sh
 docker compose build
 docker compose run jekyll
+cd _site
+scp -r * karl_leswing@karlleswing.com:/home/karl_leswing/karlleswing.com
