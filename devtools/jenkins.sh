fce0ecde3056f1d4390841d0c2a6a4a3fc180dab
Karl Leswing <karl.leswing@gmail.com>
2025-06-05 10:13:32 -0400

    Update Build

--- devtools/jenkins.sh
+++ devtools/jenkins.sh
@@ -4,4 +4,4 @@ bash devtools/set_docker_user.sh
 docker compose build
 docker compose run jekyll
 cd _site
-scp -r * karl_leswing@karlleswing.com:/home/karl_leswing/karlleswing.com
+# Site is in _site
