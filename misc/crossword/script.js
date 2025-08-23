a12a46caed1e8b0809ecfd3a5438ec053b8680af
Karl Leswing <karl.leswing@gmail.com>
2025-08-23 13:35:17 -0400

    Include

--- misc/crossword/script.js
+++ misc/crossword/script.js
@@ -1,13 +1,14 @@
 async function findMatches() {
   const query = document.getElementById("query").value.toLowerCase();
   const exclude = document.getElementById("exclude").value.toLowerCase();
+  const include = document.getElementById("include").value.toLowerCase();
   const response = await fetch("words.txt");
   const text = await response.text();
   const words = text.split("\n");
-  const matches = find_matches(words, query, exclude);
+  const matches = find_matches(words, query, exclude, include);
   document.getElementById("matches").innerText = matches.join(", ");
 }
-function find_matches(words, query, exclude) {
+function find_matches(words, query, exclude, include) {
   query = query.replace(/\*/g, ".").trim();
   const matches = [];
   const lwords = [];
@@ -21,13 +22,20 @@ function find_matches(words, query, exclude) {
       matches.push(word);
     }
   }
+  let filteredMatches = matches;
   if (exclude) {
     const excludeChars = [...new Set(exclude.split(""))];
-    return matches.filter(
+    filteredMatches = filteredMatches.filter(
       (word) => !excludeChars.some((char) => word.includes(char)),
     );
   }
-  return matches;
+  if (include) {
+    const includeChars = [...new Set(include.split(""))];
+    filteredMatches = filteredMatches.filter((word) =>
+      includeChars.every((char) => word.includes(char)),
+    );
+  }
+  return filteredMatches;
 }
 document.getElementById("query").addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
@@ -35,6 +43,11 @@ document.getElementById("query").addEventListener("keyup", function (event) {
   }
 });
 document.getElementById("exclude").addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    findMatches();
+  }
+});
+document.getElementById("include").addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
     findMatches();
   }
